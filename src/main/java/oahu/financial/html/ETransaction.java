e95c8c68cbe77978ee6ccbeec755974ec948460a
hamilton108 <hamilton108@palenque.com>
2018-04-14 08:28:11 +0200

    sellPurchase returns Page

--- src/main/java/oahu/financial/html/ETransaction.java
+++ src/main/java/oahu/financial/html/ETransaction.java
@@ -1,11 +1,13 @@
 package oahu.financial.html;
 
+import com.gargoylesoftware.htmlunit.Page;
 import oahu.financial.OptionPurchase;
 import oahu.financial.critters.Critter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public interface ETransaction {
-    void sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun) throws IOException;
+    Optional<Page> sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun) throws IOException;
 
 }
