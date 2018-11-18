8751c3ce9542352ef644c078dbc70a2e35f4e227
hamilton108 <hamilton108@palenque.com>
2018-11-18 22:45:24 +0100

    Removed interface methods (EtradeRepository)

--- src/main/java/oahu/financial/repository/EtradeRepository.java
+++ src/main/java/oahu/financial/repository/EtradeRepository.java
@@ -14,15 +14,13 @@ public interface EtradeRepository<T,T2> {
     public static enum PersistentCategory { OPTION_REF, OPTION_PRICE, STOCK_PRICE, ALL};
     Optional<DerivativePrice> findDerivativePrice(T optionInfo);
     Optional<StockPrice> stockPrice(String ticker);
-    Optional<StockPrice> stockPrice(String ticker, File suppliedFile);
     Collection<DerivativePrice> puts(String ticker);
-    Collection<DerivativePrice> puts(String ticker, File suppliedFile);
     Collection<DerivativePrice> calls(String ticker);
-    Collection<DerivativePrice> calls(String ticker, File suppliedFile);
     Collection<Derivative> callPutDefs(String ticker);
-    Collection<Derivative> callPutDefs(String ticker, File suppliedFile);
-    void saveToPersistentDataStore(PersistentCategory category);
-    void saveToPersistentDataStore(String ticker, File suppliedFile, PersistentCategory category);
+
+    //void saveToPersistentDataStore(PersistentCategory category);
+    // void saveToPersistentDataStore(String ticker, File suppliedFile, PersistentCategory category);
+
     void setDownloadDate(LocalDate localDate);
 
     Tuple3<Optional<StockPrice>,Collection<DerivativePrice>,Collection<DerivativePrice>>
