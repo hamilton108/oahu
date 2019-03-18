fc283c310347cea029a5a9f5ed80e1733b0e3dd0
Hamilton 108 <hamilton@hamilton.com>
2019-03-18 22:59:29 +0100

    Removed HtmlUnit dep

--- src/main/java/oahu/financial/html/ETransaction.java
+++ src/main/java/oahu/financial/html/ETransaction.java
@@ -1,13 +1,10 @@
 package oahu.financial.html;
 
-import com.gargoylesoftware.htmlunit.Page;
 import oahu.financial.OptionPurchase;
 import oahu.financial.critters.Critter;
 
-import java.io.IOException;
 import java.util.Optional;
 
-public interface ETransaction {
-    Optional<Page> sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun) throws IOException;
-
+public interface ETransaction<Page> {
+    Optional<Page> sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun);
 }
--- src/main/java/oahu/financial/html/WebClientManager.java
+++ src/main/java/oahu/financial/html/WebClientManager.java
@@ -1,11 +1,8 @@
 package oahu.financial.html;
 
-import com.gargoylesoftware.htmlunit.Page;
-
-import java.io.IOException;
 import java.util.Optional;
 
-public interface WebClientManager {
-    Page getPage(String url) throws IOException;
-    Optional<Page> logout() throws IOException;
+public interface WebClientManager<Page> {
+    Page getPage(String url);
+    Optional<Page> logout();
 }
