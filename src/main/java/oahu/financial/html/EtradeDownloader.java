01f58f35a64e6f81ff3e4c6379d1241f7e681b0e
Hamilton 108 <hamilton@hamilton.com>
2019-07-29 14:52:43 +0200

    EtradeDownloader TickerInfo

--- src/main/java/oahu/financial/html/EtradeDownloader.java
+++ src/main/java/oahu/financial/html/EtradeDownloader.java
@@ -3,14 +3,16 @@ package oahu.financial.html;
 import java.io.IOException;
 import java.io.Serializable;
 
-public interface EtradeDownloader<HtmlPageType, P extends Serializable> {
+public interface EtradeDownloader<HtmlPageType, TickerInfo, P extends Serializable> {
     HtmlPageType downloadDerivatives() throws IOException;
-    HtmlPageType downloadDerivatives(String ticker) throws IOException;
+    HtmlPageType downloadDerivatives(TickerInfo tickerInfo) throws IOException;
     HtmlPageType downloadIndex(String stockIndex) throws IOException;
+    /*
     HtmlPageType downloadPaperHistory(String ticker) throws IOException;
 
     HtmlPageType downloadDepth(String ticker) throws  IOException;
     HtmlPageType downloadPurchases(String ticker) throws  IOException;
+    */
     // Optional<Tuple<String>> logonParam();
     /*
     void login() throws IOException;
