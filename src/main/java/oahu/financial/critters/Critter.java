0b2dcd3640ec22b1699d3b2a7a58a7858a7b3714
Hamilton 108 <hamilton@hamilton.com>
2022-04-13 11:09:21 +0200

    Refactor critter to new package and new class names

--- src/main/java/oahu/financial/critters/Critter.java
+++
@@ -1,9 +0,0 @@
-package oahu.financial.critters;
-
-public interface Critter {
-    int getSellVolume();
-    boolean apply(SellRuleArgs args);
-    void inspect();
-    void setStatus(int value);
-}
-
--- src/main/java/oahu/financial/html/ETransaction.java
+++ src/main/java/oahu/financial/html/ETransaction.java
@@ -1,10 +1,5 @@
 package oahu.financial.html;
 
-import oahu.financial.OptionPurchase;
-import oahu.financial.critters.Critter;
-
-import java.util.Optional;
-
 public interface ETransaction<Page> {
-    Optional<Page> sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun);
+    //Optional<Page> sellPurchase(OptionPurchase purchase, Critter critter, boolean isTestRun);
 }
